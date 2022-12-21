8d5e80b81d8bc7325c83714132df778df90346f6
asyncfinkd <shamgoingzuck@gmail.com>
2022-12-22 01:53:04 +0400

    feat: sprites get more transitions and framesMax functions for draw correctly

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,11 @@
 class Sprite {
-  constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
+  constructor({
+    position,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+  }) {
     this.position = position
     this.width = 50
     this.height = 150
@@ -10,6 +16,7 @@ class Sprite {
     this.framesCurrent = 0
     this.framesElapsed = 0
     this.framesHold = 10
+    this.offset = offset
   }
 
   draw() {
@@ -19,26 +26,46 @@ class Sprite {
       0,
       this.image.width / this.framesMax,
       this.image.height,
-      this.position.x,
-      this.position.y,
+      this.position.x - this.offset.x,
+      this.position.y - this.offset.y,
       (this.image.width / this.framesMax) * this.scale,
       this.image.height * this.scale,
     )
   }
 
-  update() {
-    this.draw()
+  animateFrame() {
     this.framesElapsed++
     if (this.framesElapsed % this.framesHold === 0) {
       if (this.framesCurrent < this.framesMax - 1) this.framesCurrent++
       else this.framesCurrent = 0
     }
   }
+
+  update() {
+    this.draw()
+
+    this.animateFrame()
+  }
 }
 
-class Fighter {
-  constructor({ position, velocity, color = 'red', offset }) {
-    this.position = position
+class Fighter extends Sprite {
+  constructor({
+    position,
+    velocity,
+    color = 'red',
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+    sprites,
+  }) {
+    super({
+      position,
+      imageSrc,
+      scale,
+      framesMax,
+      offset,
+    })
     this.velocity = velocity
     this.width = 50
     this.height = 150
@@ -55,25 +82,21 @@ class Fighter {
     this.color = color
     this.isAttacking = false
     this.health = 100
-  }
+    this.framesCurrent = 0
+    this.framesElapsed = 0
+    this.framesHold = 10
+    this.sprites = sprites
 
-  draw() {
-    c.fillStyle = this.color
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-    if (this.isAttacking) {
-      c.fillStyle = 'green'
-      c.fillRect(
-        this.attackBox.position.x,
-        this.attackBox.position.y,
-        this.attackBox.width,
-        this.attackBox.height,
-      )
+    for (const sprite in this.sprites) {
+      sprites[sprite].image = new Image()
+      sprites[sprite].image.src = sprites[sprite].imageSrc
     }
   }
 
   update() {
     this.draw()
+
+    this.animateFrame()
     this.attackBox.position.x = this.position.x - this.attackBox.offset.x
     this.attackBox.position.y = this.position.y
 
@@ -82,6 +105,7 @@ class Fighter {
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
       this.velocity.y = 0
+      this.position.y = 330
     } else this.velocity.y += gravity
   }
 
@@ -91,5 +115,38 @@ class Fighter {
     setTimeout(() => {
       this.isAttacking = false
     }, 100)
+  }
+
+  switchSprite(sprite) {
+    switch (sprite) {
+      case 'idle':
+        if (this.image !== this.sprites.idle.image) {
+          this.image = this.sprites.idle.image
+          this.framesMax = this.sprites.idle.framesMax
+          this.framesCurrent = 0
+        }
+        break
+      case 'run':
+        if (this.image !== this.sprites.run.image) {
+          this.image = this.sprites.run.image
+          this.framesMax = this.sprites.run.framesMax
+          this.framesCurrent = 0
+        }
+        break
+      case 'jump':
+        if (this.image !== this.sprites.jump.image) {
+          this.image = this.sprites.jump.image
+          this.framesMax = this.sprites.jump.framesMax
+          this.framesCurrent = 0
+        }
+        break
+      case 'fall':
+        if (this.image !== this.sprites.fall.image) {
+          this.image = this.sprites.fall.image
+          this.framesMax = this.sprites.fall.framesMax
+          this.framesCurrent = 0
+        }
+        break
+    }
   }
 }
